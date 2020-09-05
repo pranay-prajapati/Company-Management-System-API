4ec0098db962935a9db74340e197e2f71e2941df
Pranay Prajapati <60659922+pranay-prajapati@users.noreply.github.com>
2020-09-05 19:54:54 +0530

    changed

--- company/employeeapi/models.py
+++ company/employeeapi/models.py
@@ -2,8 +2,12 @@ from django.db import models
 from django.core.validators import RegexValidator
 
 
-# Create your models here.
 class Employee(models.Model):
+    class Dept(models.IntegerChoices):
+        it = 1, "IT"
+        hr = 2, "HR"
+        cd = 3, "Creative Design"
+
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     email = models.EmailField(max_length=254)
@@ -12,3 +16,4 @@ class Employee(models.Model):
     contact = models.CharField(validators=[phone_regex], max_length=15, blank=True)
     dob = models.DateField(max_length=30, blank=True, null=True, default=None)
     salary = models.DecimalField(max_digits=8, decimal_places=2, null=True)
+    dept = models.PositiveSmallIntegerField(max_length=20, choices=Dept.choices, default=Dept.it)
--- company/employeeapi/serializers.py
+++ company/employeeapi/serializers.py
@@ -5,10 +5,11 @@ from datetime import datetime
 
 class EmployeeSerializers(serializers.ModelSerializer):
     age = serializers.SerializerMethodField()
+    dept_str = serializers.CharField(source='get_dept_display', read_only=True)
 
     class Meta:
         model = Employee
         fields = '__all__'
 
-    def get_age(self,edob):
+    def get_age(self, edob):
         return (datetime.now().year - edob.dob.year)
